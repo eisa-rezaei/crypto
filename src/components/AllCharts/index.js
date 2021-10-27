6fa19f3265aef393ec56d4ff0cfab47b17e6db4b
jesus re <eisa.eisa.r.r@gmail.com>
2021-10-28 00:27:54 +0330

    style res

--- src/components/AllCharts/index.js
+++ src/components/AllCharts/index.js
@@ -8,24 +8,26 @@ const CoinGeckoClient = new CoinGecko();
 const AllCharts = ({ id, isPriceUp, limit }) => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
-  const getData = useCallback(async () => {
-    setLoading(true);
-    const days = limit || 7;
-    try {
-      const res = await CoinGeckoClient.coins.fetchMarketChart(id?.toString(), {
-        days,
-        vs_currency: "usd",
-      });
-      setData(res.data);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [id, limit]);
-
   useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      const days = limit || 7;
+      try {
+        const res = await CoinGeckoClient.coins.fetchMarketChart(
+          id?.toString(),
+          {
+            days,
+            vs_currency: "usd",
+          }
+        );
+        setData(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     getData();
-  }, [getData]);
+  }, [id, limit]);
 
   const ChartHelper = (data) => {
     let ChartValues = [];
@@ -66,18 +68,22 @@ const AllCharts = ({ id, isPriceUp, limit }) => {
   // const Chartdata = (canvas) => {
   //   const ctx = canvas.getContext("2d");
   //   const gradient = ctx.createLinearGradient(0, 0, 0, 100);
-  //   gradient.addColorStop(
-  //     0,
-  //     isPriceUp ? "rgba(40, 194, 129, 0.7)" : "rgba(138, 30, 33, 0.7)"
-  //   );
-  //   gradient.addColorStop(
-  //     0.7,
-  //     isPriceUp ? "rgba(40, 194, 129, 0.2)" : "rgba(138, 30, 33, 0.2)"
-  //   );
-  //   gradient.addColorStop(
-  //     1,
-  //     isPriceUp ? "rgba(40, 194, 129, 0)" : "rgba(138, 30, 33, 0)"
-  //   );
+
+  //   gradient.addColorStop(0, "rgba(138, 30, 33, 0.8)");
+  //   gradient.addColorStop(0.7, "rgba(138, 30, 33, 0.2)");
+  //   gradient.addColorStop(1, "rgba(138, 30, 33, 0)");
+  //   // gradient.addColorStop(
+  //   //   0,
+  //   //   isPriceUp ? "rgba(40, 194, 129, 0.7)" : "rgba(167, 46, 33, 0.7)"
+  //   // );
+  //   // gradient.addColorStop(
+  //   //   0.7,
+  //   //   isPriceUp ? "rgba(40, 194, 129, 0.2)" : "rgba(167, 46, 33, 0.2)"
+  //   // );
+  //   // gradient.addColorStop(
+  //   //   1,
+  //   //   isPriceUp ? "rgba(40, 194, 129, 0)" : "rgba(167, 46, 33, 0)"
+  //   // );
   //   return {
   //     labels: ChartLabels,
   //     datasets: [
@@ -110,7 +116,7 @@ const AllCharts = ({ id, isPriceUp, limit }) => {
           percent > 0 ? "rgba(40, 194, 129, 0.2)" : "rgba(138, 30, 33, 0.2)",
         data: prices,
         borderColor:
-          percent > 0 ? "rgba(40, 194, 129, 1)" : "rgba(210, 77, 87, 1)",
+          percent > 0 ? "rgba(40, 194, 129, 1)" : "rgba(167, 46, 39, 1)",
         pointBorderWidth: 0,
         borderWidth: 3,
         tension: 0.1,
@@ -130,7 +136,7 @@ const AllCharts = ({ id, isPriceUp, limit }) => {
           <p>{percent.toFixed(2)}%</p>
         </StChartBoxContainerPrice>
         <span>
-          <Line options={options} data={Chartdata} height="100px" />
+          <Line options={options} data={Chartdata} height="100px" key={id} />
         </span>
       </StChartBoxContainer>
     );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -40,78 +40,82 @@ const Home = () => {
     return (
       <StHomeContainer>
         <StHomeCoinListContainer>
-          {state.slice(0, 5).map(({ name, id, image, symbol, market_data }) => {
-            const {
-              current_price,
-              market_cap_change_percentage_24h_in_currency: usdChange24h,
-              total_volume,
-              market_cap,
-              market_cap_rank,
-            } = market_data;
-            return (
-              <StHomeCoinListItem key={id}>
-                <CoinInfo name={name} image={image} symbol={symbol} id={id} />
-                <StHomeCoinDetail>
-                  <StHomeCoinDetailPrice isPriceUp={usdChange24h > 0}>
-                    <StHomeCoinDetailPricePart>
-                      <h5>Price</h5>
-                      <span className="price-usd">
-                        $&nbsp;
-                        {current_price.usd.toLocaleString("en")}
-                        &nbsp; <h5>USD</h5>
-                      </span>
-                      <span className="price">
-                        {current_price.btc.toLocaleString("en")}
-                        &nbsp;BTC &nbsp;&nbsp;
-                        <p>{usdChange24h.usd.toFixed(2)}%</p>
-                      </span>
-                    </StHomeCoinDetailPricePart>
+          {state
+            .slice(0, 20)
+            .map(({ name, id, image, symbol, market_data }) => {
+              const {
+                current_price,
+                market_cap_change_percentage_24h_in_currency: usdChange24h,
+                total_volume,
+                market_cap,
+                market_cap_rank,
+              } = market_data;
+              return (
+                <StHomeCoinListItem key={id}>
+                  <CoinInfo name={name} image={image} symbol={symbol} id={id} />
+                  <StHomeCoinDetail>
+                    <StHomeCoinDetailPrice isPriceUp={usdChange24h > 0}>
+                      <StHomeCoinDetailPricePart>
+                        <h5>Price</h5>
+                        <span className="price-usd">
+                          $&nbsp;
+                          {current_price.usd.toLocaleString("en")}
+                          &nbsp; <h5>USD</h5>
+                        </span>
+                        <span className="price">
+                          {current_price.btc.toLocaleString("en")}
+                          &nbsp;BTC &nbsp;&nbsp;
+                          <p>{usdChange24h.usd.toFixed(2)}%</p>
+                        </span>
+                      </StHomeCoinDetailPricePart>
 
-                    <StHomeCoinDetailPricePart>
-                      <h5>Rank</h5>
-                      <span className="rank">Rank&nbsp;{market_cap_rank}</span>
-                    </StHomeCoinDetailPricePart>
-                    <StHomeCoinDetailPricePart>
-                      <h5>Health</h5>
-                      <span>Attractive</span>
-                    </StHomeCoinDetailPricePart>
-                    <StHomeCoinDetailPricePart>
-                      <h5>sentiment</h5>
-                      <span>Attractive</span>
-                    </StHomeCoinDetailPricePart>
-                  </StHomeCoinDetailPrice>
-                  <StHomeCoinDetailPrice>
-                    <StHomeCoinDetailPricePart>
-                      <h5>Market Cap</h5>
-                      <span>
-                        $&nbsp;{market_cap.usd.toLocaleString("en")}
-                        &nbsp;USD
-                      </span>
-                      <h5>
-                        {market_cap.btc.toLocaleString("en")}
-                        &nbsp;BTC
-                      </h5>
-                    </StHomeCoinDetailPricePart>
-                    <StHomeCoinDetailPricePart>
-                      <h5>Volume</h5>
-                      <span>
-                        $&nbsp;
-                        {total_volume.usd.toLocaleString("en")}
-                        &nbsp;USD
-                      </span>
-                      <h5>
-                        {total_volume.btc.toLocaleString("en")}
-                        &nbsp;BTC
-                      </h5>
-                    </StHomeCoinDetailPricePart>
-                    <StHomeCoinDetailPricePart>
-                      <AllCharts id={id} isPriceUp={usdChange24h > 0} />
-                    </StHomeCoinDetailPricePart>
-                  </StHomeCoinDetailPrice>
-                </StHomeCoinDetail>
-              </StHomeCoinListItem>
-            );
-          })}
+                      <StHomeCoinDetailPricePart>
+                        <h5>Rank</h5>
+                        <span className="rank">
+                          Rank&nbsp;{market_cap_rank}
+                        </span>
+                      </StHomeCoinDetailPricePart>
+                      <StHomeCoinDetailPricePart>
+                        <h5>Health</h5>
+                        <span>Attractive</span>
+                      </StHomeCoinDetailPricePart>
+                      <StHomeCoinDetailPricePart>
+                        <h5>sentiment</h5>
+                        <span>Attractive</span>
+                      </StHomeCoinDetailPricePart>
+                    </StHomeCoinDetailPrice>
+                    <StHomeCoinDetailPrice>
+                      <StHomeCoinDetailPricePart>
+                        <h5>Market Cap</h5>
+                        <span>
+                          $&nbsp;{market_cap.usd.toLocaleString("en")}
+                          &nbsp;USD
+                        </span>
+                        <h5>
+                          {market_cap.btc.toLocaleString("en")}
+                          &nbsp;BTC
+                        </h5>
+                      </StHomeCoinDetailPricePart>
+                      <StHomeCoinDetailPricePart>
+                        <h5>Volume</h5>
+                        <span>
+                          $&nbsp;
+                          {total_volume.usd.toLocaleString("en")}
+                          &nbsp;USD
+                        </span>
+                        <h5>
+                          {total_volume.btc.toLocaleString("en")}
+                          &nbsp;BTC
+                        </h5>
+                      </StHomeCoinDetailPricePart>
+                      <StHomeCoinDetailPricePart>
+                        <AllCharts id={id} isPriceUp={usdChange24h > 0} />
+                      </StHomeCoinDetailPricePart>
+                    </StHomeCoinDetailPrice>
+                  </StHomeCoinDetail>
+                </StHomeCoinListItem>
+              );
+            })}
         </StHomeCoinListContainer>
       </StHomeContainer>
     );
